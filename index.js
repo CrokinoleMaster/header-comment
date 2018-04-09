7086c3426b6eeac4139bd594c3ed48bd95d42807
huarui <huarui.wu@gmail.com>
2018-04-09 16:44:17 -0700

    add consola

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const consola = require('consola')
 const path = require('path')
 const fs = require('fs')
 const readline = require('readline')
@@ -23,47 +24,43 @@ const commentMarkers = {
 
 const template = 'some kind of copyright header.\nSecond line.'
 
-glob(
-	'project/**/+(*.js|*.jsx|*.py)',
-	options,
-	(err, files) => {
-		files.forEach(fname => {
-			const extension = path.extname(fname).slice(1)
-			const marker = commentMarkers[extension]
-			const instream = fs.createReadStream(fname)
-			const outstream = new stream()
-			const rl = readline.createInterface(instream, outstream)
-			let commentLines = 0
-			rl.on('line', function(line) {
-				if (line.startsWith(marker)) {
-					commentLines++
-				} else {
-					rl.close()
-				}
-			})
+glob('project/**/+(*.js|*.jsx|*.py)', options, (err, files) => {
+	files.forEach(fname => {
+		const extension = path.extname(fname).slice(1)
+		const marker = commentMarkers[extension]
+		const instream = fs.createReadStream(fname)
+		const outstream = new stream()
+		const rl = readline.createInterface(instream, outstream)
+		let commentLines = 0
+		rl.on('line', function(line) {
+			if (line.startsWith(marker)) {
+				commentLines++
+			} else {
+				rl.close()
+			}
+		})
 
-			rl.on('close', function() {
-				if (commentLines && !NO_DELETE) {
-					commentLines++
-					// remove old header
-					shell.exec("sed -i '' 1," + commentLines + 'd ' + fname, {
-						silent: true
-					})
-				}
-				// prepend new header
-				const comment =
-					marker +
-					' ' +
-					template.replace(/\n/g, '\n' + marker + ' ') +
-					'\n\n'
-				prependFile(fname, comment, function(err) {
-					if (err) {
-						console.error('Error prepending to file: ' + fname)
-					} else {
-						console.log('added comment to file: ' + fname)
-					}
+		rl.on('close', function() {
+			if (commentLines && !NO_DELETE) {
+				commentLines++
+				// remove old header
+				shell.exec("sed -i '' 1," + commentLines + 'd ' + fname, {
+					silent: true
 				})
+			}
+			// prepend new header
+			const comment =
+				marker +
+				' ' +
+				template.replace(/\n/g, '\n' + marker + ' ') +
+				'\n\n'
+			prependFile(fname, comment, function(err) {
+				if (err) {
+					consola.error(err)
+				} else {
+					consola.info('added comment to file: ' + fname)
+				}
 			})
 		})
-	}
-)
+	})
+})
